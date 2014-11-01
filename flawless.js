1c138dc18dd9ae6dd90d36f1170f878b026663f7
KAG <k.gilhooley@gmail.com>
2014-10-31 17:13:32 -0700

    Built-out v1

--- flawless.js
+++ flawless.js
@@ -2,11 +2,59 @@
 'use strict';
 
 var shellscript = require('shellscript').globalize();
+var inquirer = require('inquirer');
 
-var check = function(){
-  shell('echo FLAWLESS');
-}
+var qTheseDiamonds = {
+  type: 'input',
+  name: 'these diamonds',
+  message: 'These diamonds?',
+  default: 'FLAWLESS'
+};
+
+var qMyDiamonds = {
+  type: 'input',
+  name: 'my diamonds',
+  message: 'My diamonds?',
+  default: 'FLAWLESS'
+};
+
+var qThisRock = {
+  type: 'input',
+  name: 'this rock',
+  message: 'This rock?',
+  default: 'FLAWLESS'
+};
+
+var qMyRock = {
+  type: 'input',
+  name: 'my rock',
+  message: 'My rock?',
+  default: 'FLAWLESS'
+};
+
+var gems = [qTheseDiamonds, qMyDiamonds, qThisRock, qMyRock];
+
+var finish = function(){
+  shell('say -v vicki I woke up like this. I woke up like this');
+};
+
+var fail = function(){
+  shell('say -v vicki Bow down, bitches');
+};
 
 module.exports = function(){
-  check();
+
+  inquirer.prompt(gems, function( answers ) {
+    if(
+      (answers['these diamonds'] === 'flawless' || answers['these diamonds'] === "FLAWLESS") &&
+      (answers['my diamonds'] === 'flawless' || answers['my diamonds'] === "FLAWLESS") &&
+      (answers['this rock'] === 'flawless' || answers['this rock'] === "FLAWLESS") &&
+      (answers['my rock'] === 'flawless' || answers['my rock'] === "FLAWLESS")
+    ){
+      finish();
+    } else {
+      fail();
+    }
+  })
+
 }();
